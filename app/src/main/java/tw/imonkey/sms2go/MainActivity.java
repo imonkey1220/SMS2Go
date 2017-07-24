6b2423c89b3714cca3bccddde3e1c266172a8001
imonkey <i0932702567@gmail.com>
2017-07-24 12:03:17 +0800

    init

--- app/src/main/java/tw/imonkey/sms2go/MainActivity.java
+++ app/src/main/java/tw/imonkey/sms2go/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         String myPhone;
         try {
             myPhone = telephonyManager.getLine1Number();
+
             sendSMS(myPhone,"SMS test.");
         } catch (Exception e) {
             e.printStackTrace();
